efcb29d42262de203f2a48952a82634cd6b8ff17
Lenny <ly2484579947@gmail.com>
2024-11-27 19:30:37 +0800

    feat: blog init

---
+++ data/headerNavLinks.ts
@@ -0,0 +1,8 @@
+const headerNavLinks = [
+  { href: '/', title: 'Home' },
+  { href: '/blog', title: '文章' },
+  { href: '/resources', title: '推荐' },
+  { href: '/about', title: '关于' },
+]
+
+export default headerNavLinks
